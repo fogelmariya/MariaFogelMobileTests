55ea53c6ac3d489fd432d4802272ed0121bff801
Artem <timon7880p@mail.ru>
2018-07-09 17:44:34 +0300

    enum edit

--- src/main/java/enums/web/Iana_org_Enum.java
+++ src/main/java/enums/web/Iana_org_Enum.java
@@ -2,14 +2,13 @@ package enums.web;
 
 public enum Iana_org_Enum {
 
-    HOME_LINK("www.iana.org/"),
-    DOMAIN_LINK("www.iana.org/domains"),
-    NUMBER_RESOURCES_LINK("www.iana.org/numbers"),
+    HOME_LINK("home_link"),
+    DOMAIN_LINK("domain_link"),
+    NUMBER_RESOURCES_LINK("mumber_resources_link"),
 
-    HOME_PAGE_TITLE("Internet Assigned Numbers Authority"),
-    DOMAIN_NAMES_TITLE("Domain Names"),
-    NUMBER_RESOURCES_TITLE("Number Resources"),
-    PROTOCOL_ASSIGNMENTS_TITLE("Protocol Assignments");
+    HOME_PAGE_TITLE("home_page_title"),
+    DOMAIN_NAMES_TITLE("domain_names_title"),
+    NUMBER_RESOURCES_TITLE("number_resources_title");
 
     public String text;
 
